a79959b96dc9c5a107a47e71b499e4a998bddcec
Isaac Thill <ithill22@turing.edu>
2023-04-17 18:56:32 -0600

    complete user story 2

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -20,5 +20,25 @@ RSpec.describe 'Dish Show Page' do
         expect(page).to have_content(@ingredient_3.name)
       end
     end
+
+    it 'I see a form to add an ingredient to the dish' do
+      visit dish_path(@dish_1)
+
+      within "#add-ingredient" do
+        expect(page).to have_content("Add Ingredient to Dish")
+        expect(page).to have_field(:ingredient_id)
+        expect(page).to have_button("Submit")
+
+        fill_in :ingredient_id, with: @ingredient_5.id
+      end
+
+      click_button "Submit"
+
+      expect(current_path).to eq(dish_path(@dish_1))
+
+      within "#ingredient-list" do
+        expect(page).to have_content(@ingredient_5.name)
+      end
+    end
   end
 end
